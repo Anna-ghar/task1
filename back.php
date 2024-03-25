c96ba29896219661e323526043319c3c3663642b
Anna-ghar <gharibyananna93@gmail.com>
2024-03-25 18:20:24 +0400

    test

---
+++ back.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('db.php');
+
+if(isset($_POST['submit'])){
+    $firstName = $_POST['fname'];
+    $lastName = $_POST['lname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $passHash = password_hash($password, PASSWORD_DEFAULT);
+
+    $errors = array();
+
+
+    if(empty($firstName) or empty($lastName) or empty($email) or empty($pass)){
+        array_push($errors, 'All fields are requared');
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        array_push($errors,'Email is not valid' );
+    }
+
+    if(strlen($pass)<8){
+        array_push($errors,'Password must be at least 8 characters long');
+    }
+
+    if(count($errors)>0){
+        foreach($errors as $error){
+            echo $error;}}
+    else {
+        $sql = "INSERT INTO user (name, surname, email, password) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        if ($stmt) {
+            $stmt->bindParam(1, $firstName);
+            $stmt->bindParam(2, $lastName);
+            $stmt->bindParam(3, $email);
+            $stmt->bindParam(4, $passHash);
+
+            if ($stmt->execute()) {
+                echo 'You registered successfully';
+            } else {
+                die('Error executing the statement');
+            }
+        } else {
+            die('Error preparing the statement');
+        }
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+<form action="back.php" method="post">
+    <label for='i1'>name</label><br>
+    <input id='i1' name='fname' type="text"><br>
+    <label for='i2'>lastname</label><br>
+    <input id='i2' name='lname' type="text"><br>
+    <label for='i3'>email</label><br>
+    <input id='i3' name='email' type="email"><br>
+    <label for='i4'>password</label><br>
+    <input id='i4' name='pass' type="password"><br><br>
+    <input type="submit" name="submit">
+</form>
+</body>
+</html>
+
